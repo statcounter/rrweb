4c305f5a8117fd6ecd1d893693f031190a1cf20c
eoghan <eoghan@getthere.ie>
2020-01-14 10:25:50 +0000

    Also need to produce a packed version of rrweb-record.js as our own packing process doesn't handle things like `const` and `let`

--- sc-build.sh
+++ sc-build.sh
@@ -1,4 +1,7 @@
 cd rrweb-snapshot
 npm run bundle && dos2unix dist/*
 cd -
-npm run bundle:browser-unpacked && dos2unix dist/record/* dist/* && cp -f /home/statcounter/rrweb/dist/rrweb.js /home/statcounter/www/js/rrweb/rrweb.js && cp -f /home/statcounter/rrweb/dist/record/rrweb-record.js /home/statcounter/www/js/rrweb/rrweb-record.js
+npm run bundle:browser-record && dos2unix dist/record/* dist/*
+cp -f /home/statcounter/rrweb/dist/rrweb.js /home/statcounter/www/js/rrweb/rrweb.js
+cp -f /home/statcounter/rrweb/dist/record/rrweb-record.js /home/statcounter/www/js/rrweb/rrweb-record.js
+cp -f /home/statcounter/rrweb/dist/record/rrweb-record.min.js /home/statcounter/www/js/rrweb/rrweb-record.min.js
