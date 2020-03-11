fa0c74922fa21eb7a8458cd636b6e365e309dbee
eoghan <eoghan@getthere.ie>
2020-03-11 13:07:45 +0000

    We're not packing rrweb.js on our end but concatenating it - r28889

--- sc-build.sh
+++ sc-build.sh
@@ -3,5 +3,6 @@ npm run bundle && dos2unix dist/*
 cd -
 npm run bundle:browser-record && dos2unix dist/record/* dist/*
 cp -f /home/statcounter/rrweb/dist/rrweb.js /home/statcounter/www/js/rrweb/rrweb.js
+cp -f /home/statcounter/rrweb/dist/rrweb.min.js /home/statcounter/www/js/rrweb/rrweb.min.js
 cp -f /home/statcounter/rrweb/dist/record/rrweb-record.js /home/statcounter/www/js/rrweb/rrweb-record.js
 cp -f /home/statcounter/rrweb/dist/record/rrweb-record.min.js /home/statcounter/www/js/rrweb/rrweb-record.min.js
