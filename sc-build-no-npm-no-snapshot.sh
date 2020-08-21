4070db395882094c38ba922672f593b15dcb8efa
eoghan <eoghan@getthere.ie>
2020-08-21 15:44:19 +0000

    Some extra versioning we are doing to ensure correct rrweb makes it into production

--- sc-build-no-npm-no-snapshot.sh
+++ sc-build-no-npm-no-snapshot.sh
@@ -4,3 +4,10 @@ cp -f /home/statcounter/rrweb/dist/replay/rrweb-replay.js /home/statcounter/www/
 cp -f /home/statcounter/rrweb/dist/replay/rrweb-replay.min.js /home/statcounter/www/libs/rrweb/rrweb-replay.min.js
 cp -f /home/statcounter/rrweb/dist/record/rrweb-record.js /home/statcounter/www/libs/rrweb/rrweb-record.js
 cp -f /home/statcounter/rrweb/dist/record/rrweb-record.min.js /home/statcounter/www/libs/rrweb/rrweb-record.min.js
+RRWEBV=`git rev-parse HEAD`
+cd rrweb-snapshot
+RRWEBSV=`git rev-parse HEAD`
+cd -
+mkdir -p /home/statcounter/www/libs/rrweb/versioned
+cp -f /home/statcounter/rrweb/dist/record/rrweb-record.js "/home/statcounter/www/libs/rrweb/versioned/rrweb-record-$RRWEBV-$RRWEBSV.js"
+cp -f /home/statcounter/rrweb/dist/record/rrweb-record.min.js "/home/statcounter/www/libs/rrweb/versioned/rrweb-record-$RRWEBV-$RRWEBSV.min.js"
