f629932d9eb973a6a4676da6081a5e6143a787f7
Eoghan Murray <eoghan@getthere.ie>
2021-01-20 15:54:35 +0000

    More compact storage for the much more common attribute value without an `!important` flag - saves 6 chars per style attr in the json :)

--- src/types.ts
+++ src/types.ts
@@ -282,7 +282,7 @@ export type textMutation = {
 };
 
 export type styleAttributeValue = {
-  [key:string]: [string, string] | false;
+  [key:string]: [string, string] | string | false;
 };
 
 export type attributeCursor = {
