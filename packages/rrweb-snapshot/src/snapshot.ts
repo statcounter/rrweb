fa6e36be338b200be12119a0395f016e6e84cacd
Eoghan Murray <eoghan@getthere.ie>
2021-07-28 16:40:44 +0100

    Save the original href (needed to e.g. track the evolution of the same block of CSS content between versions)

--- packages/rrweb-snapshot/src/snapshot.ts
+++ packages/rrweb-snapshot/src/snapshot.ts
@@ -660,6 +660,9 @@ function serializeElementNode(
     const attr = n.attributes[i];
     if (attributes._cssText && ['href', 'rel'].includes(attr.name)) {
       // ignore in snapshot, and no need to try to asset capture the href (already done)
+      if (attr.name === 'href') {
+        attributes._originalHref = value;
+      }
     } else if (!ignoreAttribute(tagName, attr.name, attr.value) && !needBlock) {
       const value = transformAttribute(
         doc,
