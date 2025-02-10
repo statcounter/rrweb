0c191797e8ba3f001dc9ac8922214c197449caae
Eoghan Murray <eoghan@getthere.ie>
2025-02-10 10:21:37 +0000

    Allow record to be built on it's own

---
+++ sc-build-record.sh
@@ -0,0 +1,23 @@
+cd packages/record
+yarn build
+dos2unix -q dist/record/*
+cd - &> /dev/null
+
+if ! [[ -n $(diff /home/statcounter/rrweb/packages/record/dist/record.umd.cjs /home/statcounter/www/libs/rrweb/rrweb-record.js) ]]; then
+  # don't take next actions if only replay stuff has changed
+   echo 'record unchanged'
+   #exit 0
+fi
+
+cp -f /home/statcounter/rrweb/packages/record/dist/record.umd.cjs /home/statcounter/www/libs/rrweb/rrweb-record.js
+cp -f /home/statcounter/rrweb/packages/record/dist/record.umd.min.cjs /home/statcounter/www/libs/rrweb/rrweb-record.min.js
+sed -i "/^\/\/# sourceMappingURL/d" --  /home/statcounter/www/libs/rrweb/rrweb-record.min.js
+RRWEBV=`git rev-parse HEAD`
+mkdir -p /home/statcounter/www/libs/rrweb/versioned
+cp -f /home/statcounter/rrweb/packages/record/dist/record.umd.cjs "/home/statcounter/www/libs/rrweb/versioned/rrweb-record-$RRWEBV.js"
+cp -f /home/statcounter/rrweb/packages/record/dist/record.umd.min.cjs "/home/statcounter/www/libs/rrweb/versioned/rrweb-record-$RRWEBV.min.js"
+sed -i "s|var rrweb_version = '.*';  // DEBUG|var rrweb_version = '$RRWEBV';  // DEBUG|g" -- /home/statcounter/www/counter/recorder_test_uncompressed.js
+touch /home/statcounter/www/counter/recorder_test_uncompressed.js
+cd ..
+./pack_counter.py --rrweb --test-only
+cd - &> /dev/null
